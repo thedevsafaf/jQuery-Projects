ebb585d5f54f5e1221b4066abf4906a2bad98070
Mohammed Safaf <85129653+thedevsafaf@users.noreply.github.com>
2021-08-20 02:37:22 +0530

    Add files via upload

---
+++ gallery-image-popup-using-jquery-plugin/js/script.js
@@ -0,0 +1,17 @@
+function resize(){
+
+}
+
+$(document).ready(function(){
+    resize();
+});
+  
+$(window).resize(function(){
+    resize();
+});
+
+$(window).on('load',function(){
+    resize();
+});
+
+
